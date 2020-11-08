59831b0723f4c11bd20271df875961bec9b19086
russelj <russelj@rose-hulman.edu>
2020-11-08 11:48:30 -0500

    Just commented out the code with errors so I could make our demo video

--- HackHolyoke_Project/src/interfaces/NumberScore.java
+++ HackHolyoke_Project/src/interfaces/NumberScore.java
@@ -7,36 +7,36 @@ import interfaces.Subtraction;
 
 public class NumberScore {
 
-		//place to call on the addition and subtraction interfaces when an operand is run
-	public NumberScore() {
-	public int num=0;
-	public char op ='d';
-	}
-	/*
-	 * we find out the three things the bird runs into.
-	 */
-	if(num) {
-		int a = num;
-	}
-	//look for bird to run into another number and then update the num integer to that number
-	if(num) {
-		int b=num;
-	}
-	//depending on the operand we will call the respectful method
-	if(op) {
-		if(op=='+') {
-			//call addition method
-		}
-		if(op=='*') {
-			//call product method
-		}
-		if(op=='-') {
-			//call subtraction method
-		}
-		if(op=='/') {
-			//call divide method
-		}
-		
-	}
+//		//place to call on the addition and subtraction interfaces when an operand is run
+//	public NumberScore() {
+//	public int num=0;
+//	public char op ='d';
+//	}
+//	/*
+//	 * we find out the three things the bird runs into.
+//	 */
+//	if(num) {
+//		int a = num;
+//	}
+//	//look for bird to run into another number and then update the num integer to that number
+//	if(num) {
+//		int b=num;
+//	}
+//	//depending on the operand we will call the respectful method
+//	if(op) {
+//		if(op=='+') {
+//			//call addition method
+//		}
+//		if(op=='*') {
+//			//call product method
+//		}
+//		if(op=='-') {
+//			//call subtraction method
+//		}
+//		if(op=='/') {
+//			//call divide method
+//		}
+//		
+//	}
 
 }
