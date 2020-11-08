9458646aa4748f0248cc8fe34baf67cca327a1e0
afsaanabhatt <afsaanabhatt@afsaanas-air>
2020-11-08 01:11:33 -0800

    An idea of what we can do in the commented part

--- HackHolyoke_Project/src/interfaces/Subtraction.java
+++ HackHolyoke_Project/src/interfaces/Subtraction.java
@@ -2,4 +2,12 @@ package interfaces;
 
 public class Subtraction {
 
+	public int subracting(int a, int b) {
+		int c=a-b;
+		return c;
+	}
+	public int division(int a,int b) {
+		int c=a/b;
+		return c;
+	}
 }
--- HackHolyoke_Project/src/interfaces/addition.java
+++ HackHolyoke_Project/src/interfaces/addition.java
@@ -1,5 +1,13 @@
 package interfaces;
 
 public class addition {
-		
+	
+	public int add(int a, int b) {
+		int c = a+b;
+		return c;
+	}
+	public int product(int a, int b) {
+		int c = a*b;
+		return c;
+	}
 }
